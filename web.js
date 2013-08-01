dc29231d6eb7d26d304102f1f8d179f3b6158e88
Steffen Blenk <steffenblenk@gmx.de>
2013-08-01 15:25:08 +0000

    Buffer Versuch1

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var buffer = new Buffer(fs.readFileSync('index.html'));
+  //response.send('Hello World 2!');
+  response.send(buffer.toString('utf8' );
 });
 
 var port = process.env.PORT || 5000;
